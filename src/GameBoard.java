dcdedbe8c830b98f5bd5f837354e90cfc1a87221
Aaron Harvey <Harvey.AaronJ@gmail.com>
2023-04-02 13:16:52 -0400

    .

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -23,5 +23,6 @@ public class GameBoard {
             System.out.println();
         }
     }
-    
+
+   
 }
--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -9,6 +9,7 @@ public class TicTacToe {
 
     public static void main(String[] args) {
         GameBoard.printGameBoard();
+        GameBoard.test();
 
         // Print a game board of 3 rows of 3 colums to the console. 
         // The player enters a number 1-9 to place their marker in that spot
