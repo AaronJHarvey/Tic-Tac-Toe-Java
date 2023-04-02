a06f31fb4146fa68496fa8a4caec9ac3e8ca5efb
Aaron Harvey <Harvey.AaronJ@gmail.com>
2023-04-02 11:35:46 -0400

    Creates GameBoard and runs for loop on it.

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,5 +1,27 @@
 public class TicTacToe {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        char[][] gameBoard = {
+            {' ', '|', ' ', '|', ' '},
+            {'-', '+', '-', '+', '-'},
+            {' ', '|', ' ', '|', ' '},
+            {'-', '+', '-', '+', '-'},
+            {' ', '|', ' ', '|', ' '}
+        };
+
+        // for each row in the gameBoard (char[] row)
+        for (char[] row : gameBoard) {
+            // for each char c in the row (char character)
+            for (char c : row) {
+                System.out.print(c);
+            }
+        }
+        
+        
+        // Print a game board of 3 rows of 3 colums to the console. 
+        // The player enters a number 1-9 to place their marker in that spot
+        // computer does the same
+        // This continues until one player/computer gets 3 of their markers in a row or all spaces are filled.
+
+    
     }
 }
