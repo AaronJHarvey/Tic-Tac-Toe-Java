a0c86e98a1554b63a8e4c152c585af88b60f4b4c
Aaron Harvey <Harvey.AaronJ@gmail.com>
2023-04-02 12:27:56 -0400

    Moves the Gameboard to it's own file

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,28 +1,19 @@
 public class TicTacToe {
-    public static void main(String[] args) {
-        char[][] gameBoard = {
-            {' ', '|', ' ', '|', ' '},
-            {'-', '+', '-', '+', '-'},
-            {' ', '|', ' ', '|', ' '},
-            {'-', '+', '-', '+', '-'},
-            {' ', '|', ' ', '|', ' '}
-        };
+    // public static char[][] gameBoard = {
+    //         { ' ', '|', ' ', '|', ' ' },
+    //         { '-', '+', '-', '+', '-' },
+    //         { ' ', '|', ' ', '|', ' ' },
+    //         { '-', '+', '-', '+', '-' },
+    //         { ' ', '|', ' ', '|', ' ' }
+    // }; // Line one: empty space, space border line, empty space, space border line, empty space.
 
-        // for each row in the gameBoard (char[] row)
-        for (char[] row : gameBoard) {
-            // for each char c in the row (char character)
-            for (char c : row) {
-                System.out.print(c);
-            }
-            System.out.println();
-        }
-        
-        
+    public static void main(String[] args) {
+        GameBoard.printGameBoard();
+
         // Print a game board of 3 rows of 3 colums to the console. 
         // The player enters a number 1-9 to place their marker in that spot
         // computer does the same
         // This continues until one player/computer gets 3 of their markers in a row or all spaces are filled.
 
-    
     }
 }
