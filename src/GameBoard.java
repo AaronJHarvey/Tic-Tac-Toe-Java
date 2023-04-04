e33cbf46ead97b36c2196d146b4508d51486b83d
Aaron Harvey <Harvey.AaronJ@gmail.com>
2023-04-04 10:02:55 -0400

    Changes things are defined

--- src/GameBoard.java
+++ src/GameBoard.java
@@ -1,18 +1,14 @@
 public class GameBoard {
-    public static char[][] gameBoard = {
-            //  First Line/Row: Empty Space, Space Border Line, Empty Space, Space Border line, Empty Space
-            //  Second Line/Row - All borders for board: Lower/Upper space border line, Cross Line where 4 spaces intersect,Lower/Upper space border line, Cross Line where 4 spaces intersect, Lower/Upper Space border line 
-            //  Third Line/Row: Empty Space, Space Border Line, Empty Space, Space Border line, Empty Space
-            //  Fourth Line/Row - All borders for board: Lower/Upper space border line, Cross Line where 4 spaces intersect,Lower/Upper space border line, Cross Line where 4 spaces intersect, Lower/Upper Space border line 
-            //  Fifth Line/Row: Empty Space, Space Border Line, Empty Space, Space Border line, Empty Space
-            { ' ', '|', ' ', '|', ' ' },
-            { '-', '+', '-', '+', '-' },
-            { ' ', '|', ' ', '|', ' ' },
-            { '-', '+', '-', '+', '-' },
-            { ' ', '|', ' ', '|', ' ' }
-    };
+    // public static char[][] gameBoard = {
+
+    //         { ' ', '|', ' ', '|', ' ' },
+    //         { '-', '+', '-', '+', '-' },
+    //         { ' ', '|', ' ', '|', ' ' },
+    //         { '-', '+', '-', '+', '-' },
+    //         { ' ', '|', ' ', '|', ' ' }
+    // };
     
-    public static void printGameBoard() {
+    public static void printGameBoard(char[][] gameBoard) {
 
         // for each row in the gameBoard (char[] row)
         for (char[] row : gameBoard) {
--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,15 +1,18 @@
 public class TicTacToe {
-    // public static char[][] gameBoard = {
-    //         { ' ', '|', ' ', '|', ' ' },
-    //         { '-', '+', '-', '+', '-' },
-    //         { ' ', '|', ' ', '|', ' ' },
-    //         { '-', '+', '-', '+', '-' },
-    //         { ' ', '|', ' ', '|', ' ' }
-    // }; // Line one: empty space, space border line, empty space, space border line, empty space.
-
+    //  First Line/Row: Empty Space, Space Border Line, Empty Space, Space Border line, Empty Space
+    //  Second Line/Row - All borders for board: Lower/Upper space border line, Cross Line where 4 spaces intersect,Lower/Upper space border line, Cross Line where 4 spaces intersect, Lower/Upper Space border line 
+    //  Third Line/Row: Empty Space, Space Border Line, Empty Space, Space Border line, Empty Space
+    //  Fourth Line/Row - All borders for board: Lower/Upper space border line, Cross Line where 4 spaces intersect,Lower/Upper space border line, Cross Line where 4 spaces intersect, Lower/Upper Space border line 
+    //  Fifth Line/Row: Empty Space, Space Border Line, Empty Space, Space Border line, Empty Space
+    public static char[][] gameBoard = {
+            { ' ', '|', ' ', '|', ' ' },
+            { '-', '+', '-', '+', '-' },
+            { ' ', '|', ' ', '|', ' ' },
+            { '-', '+', '-', '+', '-' },
+            { ' ', '|', ' ', '|', ' ' }
+    }; 
     public static void main(String[] args) {
-        GameBoard.printGameBoard();
-        GameBoard.test();
+        GameBoard.printGameBoard(gameBoard);
 
         // Print a game board of 3 rows of 3 colums to the console. 
         // The player enters a number 1-9 to place their marker in that spot
