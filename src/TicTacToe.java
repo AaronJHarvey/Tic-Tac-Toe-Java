6d77b809f71fe33aa54472bf905a02eee53ca05a
Aaron Harvey <Harvey.AaronJ@gmail.com>
2023-04-06 11:37:01 -0400

    .

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,55 +1,67 @@
 import java.util.Scanner;
 
 public class TicTacToe {
-  
+
     public static void main(String[] args) {
 
-        GameBoard.printGameBoard();
+        char[][] gameBoard = {
+
+                //  First Line/Row: Empty Space, Space Border Line, Empty Space, Space Border line, Empty Space
+                //  Second Line/Row - All borders for board: Lower/Upper space border line, Cross Line where 4 spaces intersect,Lower/Upper space border line, Cross Line where 4 spaces intersect, Lower/Upper Space border line 
+                //  Third Line/Row: Empty Space, Space Border Line, Empty Space, Space Border line, Empty Space
+                //  Fourth Line/Row - All borders for board: Lower/Upper space border line, Cross Line where 4 spaces intersect,Lower/Upper space border line, Cross Line where 4 spaces intersect, Lower/Upper Space border line 
+                //  Fifth Line/Row: Empty Space, Space Border Line, Empty Space, Space Border line, Empty Space
+
+                { ' ', '|', ' ', '|', ' ' },
+                { '-', '+', '-', '+', '-' },
+                { ' ', '|', ' ', '|', ' ' },
+                { '-', '+', '-', '+', '-' },
+                { ' ', '|', ' ', '|', ' ' }
+        };
+
+        printGameBoard(gameBoard);
+        placeMarker();
+    }
+
+    public static void printGameBoard(char[][] gameBoard) {
+
+        // for each row in the gameBoard (char[] row)
+        for (char[] row : gameBoard) {
+            // for each char c in the row (char character)
+            for (char c : row) {
+                System.out.print(c);
+            }
+            System.out.println();
+        }
+    }
+
+    public static void placeMarker() {
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Enter Your Placement (1-9): ");
+        int input = scan.nextInt();
+        System.out.println(input);
         
-
-        
-        // Scanner scan = new Scanner(System.in);
-        // System.out.print("Enter your Placement (1-9): ");
-        // int pos = scan.nextInt();
-        // System.out.println(pos);
-        
-        // switch (pos) {
-        //     case 1:
-        //         board[0][0] = 'x';
-        //         break;
-        //     case 2:
-        //         board[0][2] = 'x';
-        //         break;
-        //     case 3:
-        //         board[0][4] = 'x';
-        //         break;
-        //     case 4:
-        //         board[2][0] = 'x';
-        //         break;
-        //     case 5:
-        //         board[2][2] = 'x';
-        //         break;
-        //     case 6:
-        //         board[2][4] = 'x';
-        //         break;
-        //     case 7:
-        //         board[4][0] = 'x';
-        //         break; 
-        //     case 8:
-        //         board[4][2] = 'x';
-        //         break;
-        //     case 9:
-        //         board[4][4] = 'x';
-        //         break;
-        // }
-        // GameBoard.printGameBoard();
-
-
-
-        // Print a game board of 3 rows of 3 colums to the console. 
-        // The player enters a number 1-9 to place their marker in that spot
-        // computer does the same
-        // This continues until one player/computer gets 3 of their markers in a row or all spaces are filled.
-
+        switch (input) {
+            case 1:
+                gameBoard[0][0] = "x";
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            case 7:
+                break;
+            case 8:
+                break;
+            case 9:
+                break;
+            
+        }
     }
 }
