26e797302b6044d85f5c513411e28dfb1081b84a
Aaron Harvey <Harvey.AaronJ@gmail.com>
2023-04-06 11:43:43 -0400

    Adds switch statement that adds marker

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -12,15 +12,15 @@ public class TicTacToe {
                 //  Fourth Line/Row - All borders for board: Lower/Upper space border line, Cross Line where 4 spaces intersect,Lower/Upper space border line, Cross Line where 4 spaces intersect, Lower/Upper Space border line 
                 //  Fifth Line/Row: Empty Space, Space Border Line, Empty Space, Space Border line, Empty Space
 
-                { ' ', '|', ' ', '|', ' ' },
+                { '1', '|', '2', '|', '3' },
                 { '-', '+', '-', '+', '-' },
-                { ' ', '|', ' ', '|', ' ' },
+                { '4', '|', '5', '|', '6' },
                 { '-', '+', '-', '+', '-' },
-                { ' ', '|', ' ', '|', ' ' }
+                { '7', '|', '8', '|', '9' }
         };
 
         printGameBoard(gameBoard);
-        placeMarker();
+        placeMarker(gameBoard);
     }
 
     public static void printGameBoard(char[][] gameBoard) {
@@ -35,31 +35,48 @@ public class TicTacToe {
         }
     }
 
-    public static void placeMarker() {
+    public static void placeMarker(char[][] gameBoard) {
         Scanner scan = new Scanner(System.in);
         System.out.print("Enter Your Placement (1-9): ");
         int input = scan.nextInt();
-        System.out.println(input);
+
         
         switch (input) {
             case 1:
-                gameBoard[0][0] = "x";
+                gameBoard[0][0] = 'x';
+                printGameBoard(gameBoard);
                 break;
             case 2:
+                gameBoard[0][2] = 'x';
+                printGameBoard(gameBoard);
                 break;
             case 3:
+                gameBoard[0][4] = 'x';
+                printGameBoard(gameBoard);
                 break;
             case 4:
+                gameBoard[2][0] = 'x';
+                printGameBoard(gameBoard);
                 break;
             case 5:
+                gameBoard[2][2] = 'x';
+                printGameBoard(gameBoard);
                 break;
             case 6:
+                gameBoard[2][4] = 'x';
+                printGameBoard(gameBoard);
                 break;
             case 7:
+                gameBoard[4][0] = 'x';
+                printGameBoard(gameBoard);
                 break;
             case 8:
+                gameBoard[4][2] = 'x';
+                printGameBoard(gameBoard);
                 break;
             case 9:
+                gameBoard[4][4] = 'x';
+                printGameBoard(gameBoard);
                 break;
             
         }
